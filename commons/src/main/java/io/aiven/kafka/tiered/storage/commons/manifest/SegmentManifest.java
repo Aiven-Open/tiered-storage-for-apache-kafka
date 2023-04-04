49899ab57d61977af05e34dc2cc2fbfb42c7bd89
Ivan Yurchenko <ivanyu@aiven.io>
2023-04-04 08:33:10 +0300

    Add segment manifest and its (de-)serialization
    This includes also a secret encryption/decryption on serialization/deserialization.

---
+++ commons/src/main/java/io/aiven/kafka/tiered/storage/commons/manifest/SegmentManifest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2023 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.tiered.storage.commons.manifest;
+
+import java.util.Optional;
+
+import io.aiven.kafka.tiered.storage.commons.chunkindex.ChunkIndex;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+/**
+ * The segment manifest.
+ *
+ * <p>Contains various metadata about an uploaded segment.
+ */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "version")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = SegmentManifestV1.class, name = "1")
+})
+public interface SegmentManifest {
+    ChunkIndex chunkIndex();
+
+    boolean compression();
+
+    Optional<SegmentEncryptionMetadata> encryption();
+}
