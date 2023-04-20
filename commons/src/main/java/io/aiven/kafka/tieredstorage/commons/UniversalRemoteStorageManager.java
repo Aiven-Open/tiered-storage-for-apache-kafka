87307785f18483ffe0e1bcc48219e86df46376c8
Anatolii Popov <anatolii.popov@aiven.io>
2023-04-20 15:55:14 +0300

    UniversalRemoteStorageManager implementation

--- commons/src/main/java/io/aiven/kafka/tieredstorage/commons/UniversalRemoteStorageManager.java
+++ commons/src/main/java/io/aiven/kafka/tieredstorage/commons/UniversalRemoteStorageManager.java
@@ -16,54 +16,223 @@
 
 package io.aiven.kafka.tieredstorage.commons;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.nio.file.Files;
 import java.util.Map;
+import java.util.Objects;
 
+import org.apache.kafka.common.utils.ByteBufferInputStream;
 import org.apache.kafka.server.log.remote.storage.LogSegmentData;
 import org.apache.kafka.server.log.remote.storage.RemoteLogSegmentMetadata;
 import org.apache.kafka.server.log.remote.storage.RemoteStorageException;
 import org.apache.kafka.server.log.remote.storage.RemoteStorageManager;
 
+import io.aiven.kafka.tieredstorage.commons.manifest.SegmentEncryptionMetadataV1;
+import io.aiven.kafka.tieredstorage.commons.manifest.SegmentManifest;
+import io.aiven.kafka.tieredstorage.commons.manifest.SegmentManifestV1;
+import io.aiven.kafka.tieredstorage.commons.manifest.index.ChunkIndex;
+import io.aiven.kafka.tieredstorage.commons.manifest.serde.DataKeyDeserializer;
+import io.aiven.kafka.tieredstorage.commons.manifest.serde.DataKeySerializer;
+import io.aiven.kafka.tieredstorage.commons.security.AesEncryptionProvider;
+import io.aiven.kafka.tieredstorage.commons.security.DataKeyAndAAD;
+import io.aiven.kafka.tieredstorage.commons.security.RsaEncryptionProvider;
+import io.aiven.kafka.tieredstorage.commons.storage.ObjectStorageFactory;
+import io.aiven.kafka.tieredstorage.commons.transform.BaseTransformChunkEnumeration;
+import io.aiven.kafka.tieredstorage.commons.transform.CompressionChunkEnumeration;
+import io.aiven.kafka.tieredstorage.commons.transform.EncryptionChunkEnumeration;
+import io.aiven.kafka.tieredstorage.commons.transform.FetchChunkEnumeration;
+import io.aiven.kafka.tieredstorage.commons.transform.TransformChunkEnumeration;
+import io.aiven.kafka.tieredstorage.commons.transform.TransformFinisher;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+
+import static org.apache.kafka.server.log.remote.storage.RemoteStorageManager.IndexType.LEADER_EPOCH;
+import static org.apache.kafka.server.log.remote.storage.RemoteStorageManager.IndexType.OFFSET;
+import static org.apache.kafka.server.log.remote.storage.RemoteStorageManager.IndexType.PRODUCER_SNAPSHOT;
+import static org.apache.kafka.server.log.remote.storage.RemoteStorageManager.IndexType.TIMESTAMP;
+import static org.apache.kafka.server.log.remote.storage.RemoteStorageManager.IndexType.TRANSACTION;
+
 public class UniversalRemoteStorageManager implements RemoteStorageManager {
+    private UniversalRemoteStorageManagerConfig config;
+
+    private ObjectStorageFactory objectStorageFactory;
+    private boolean compression;
+    private boolean encryption;
+    private int chunkSize;
+    private RsaEncryptionProvider rsaEncryptionProvider;
+    private AesEncryptionProvider aesEncryptionProvider;
+    private ObjectMapper mapper;
+    private ChunkManager chunkManager;
+    private ObjectKey objectKey;
+
     @Override
     public void configure(final Map<String, ?> configs) {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(configs, "configs must not be null");
+        config = new UniversalRemoteStorageManagerConfig(configs);
+        objectStorageFactory = config.objectStorageFactory();
+        objectKey = new ObjectKey(config.keyPrefix());
+        encryption = config.encryptionEnabled();
+        if (encryption) {
+            rsaEncryptionProvider = RsaEncryptionProvider.of(
+                config.encryptionPublicKeyFile(),
+                config.encryptionPrivateKeyFile()
+            );
+            aesEncryptionProvider = new AesEncryptionProvider();
+        }
+        chunkManager = new ChunkManager(objectStorageFactory, objectKey, aesEncryptionProvider);
+        chunkSize = config.chunkSize();
+        compression = config.compressionEnabled();
+
+        mapper = getObjectMapper();
+    }
+
+    private ObjectMapper getObjectMapper() {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Jdk8Module());
+        if (encryption) {
+            final SimpleModule simpleModule = new SimpleModule();
+            simpleModule.addSerializer(SecretKey.class, new DataKeySerializer(rsaEncryptionProvider::encryptDataKey));
+            simpleModule.addDeserializer(SecretKey.class, new DataKeyDeserializer(
+                b -> new SecretKeySpec(rsaEncryptionProvider.decryptDataKey(b), "AES")));
+            objectMapper.registerModule(simpleModule);
+        }
+        return objectMapper;
     }
 
     @Override
     public void copyLogSegmentData(final RemoteLogSegmentMetadata remoteLogSegmentMetadata,
                                    final LogSegmentData logSegmentData) throws RemoteStorageException {
-        throw new UnsupportedOperationException();
+        Objects.requireNonNull(remoteLogSegmentMetadata, "remoteLogSegmentId must not be null");
+        Objects.requireNonNull(logSegmentData, "logSegmentData must not be null");
+        try {
+            TransformChunkEnumeration transformEnum = new BaseTransformChunkEnumeration(
+                Files.newInputStream(logSegmentData.logSegment()), chunkSize);
+            SegmentEncryptionMetadataV1 encryptionMetadata = null;
+            if (compression) {
+                transformEnum = new CompressionChunkEnumeration(transformEnum);
+            }
+            if (encryption) {
+                final DataKeyAndAAD dataKeyAndAAD = aesEncryptionProvider.createDataKeyAndAAD();
+                transformEnum = new EncryptionChunkEnumeration(
+                    transformEnum,
+                    () -> aesEncryptionProvider.encryptionCipher(dataKeyAndAAD));
+                encryptionMetadata = new SegmentEncryptionMetadataV1(dataKeyAndAAD.dataKey, dataKeyAndAAD.aad);
+            }
+            final var transformFinisher =
+                new TransformFinisher(transformEnum, remoteLogSegmentMetadata.segmentSizeInBytes());
+            try (final var sis = new SequenceInputStream(transformFinisher)) {
+                final String fileKey =
+                    objectKey.key(remoteLogSegmentMetadata, ObjectKey.Suffix.LOG);
+                objectStorageFactory.fileUploader().upload(sis, fileKey);
+            }
+
+
+            final ChunkIndex chunkIndex = transformFinisher.chunkIndex();
+            final SegmentManifest segmentManifest =
+                new SegmentManifestV1(chunkIndex, compression, encryptionMetadata);
+            uploadManifest(remoteLogSegmentMetadata, segmentManifest);
+
+
+            uploadIndexFile(remoteLogSegmentMetadata, Files.newInputStream(logSegmentData.offsetIndex()), OFFSET);
+            uploadIndexFile(remoteLogSegmentMetadata, Files.newInputStream(logSegmentData.timeIndex()),
+                TIMESTAMP);
+            uploadIndexFile(remoteLogSegmentMetadata, Files.newInputStream(logSegmentData.producerSnapshotIndex()),
+                PRODUCER_SNAPSHOT);
+            if (logSegmentData.transactionIndex().isPresent()) {
+                uploadIndexFile(remoteLogSegmentMetadata, Files.newInputStream(logSegmentData.transactionIndex().get()),
+                    TRANSACTION);
+            }
+            uploadIndexFile(remoteLogSegmentMetadata, new ByteBufferInputStream(logSegmentData.leaderEpochIndex()),
+                LEADER_EPOCH);
+        } catch (final IOException e) {
+            throw new RemoteStorageException(e);
+        }
+    }
+
+    private void uploadIndexFile(final RemoteLogSegmentMetadata remoteLogSegmentMetadata,
+                                 final InputStream index,
+                                 final IndexType indexType) throws IOException {
+        objectStorageFactory.fileUploader().upload(index,
+            objectKey.key(remoteLogSegmentMetadata, ObjectKey.Suffix.fromIndexType(indexType)));
+    }
+
+    private void uploadManifest(final RemoteLogSegmentMetadata remoteLogSegmentMetadata,
+                                final SegmentManifest segmentManifest)
+        throws IOException {
+        final String manifest = mapper.writeValueAsString(segmentManifest);
+        final String manifestFileKey =
+            objectKey.key(remoteLogSegmentMetadata, ObjectKey.Suffix.MANIFEST);
+
+        objectStorageFactory.fileUploader().upload(new ByteArrayInputStream(manifest.getBytes()), manifestFileKey);
     }
 
     @Override
     public InputStream fetchLogSegment(final RemoteLogSegmentMetadata remoteLogSegmentMetadata,
                                        final int startPosition) throws RemoteStorageException {
-        throw new UnsupportedOperationException();
+        return this.fetchLogSegment(remoteLogSegmentMetadata, startPosition,
+            remoteLogSegmentMetadata.segmentSizeInBytes());
     }
 
     @Override
     public InputStream fetchLogSegment(final RemoteLogSegmentMetadata remoteLogSegmentMetadata,
                                        final int startPosition,
                                        final int endPosition) throws RemoteStorageException {
-        throw new UnsupportedOperationException();
+        try {
+            final InputStream manifest = objectStorageFactory.fileFetcher()
+                .fetch(objectKey.key(remoteLogSegmentMetadata, ObjectKey.Suffix.MANIFEST));
+
+            final SegmentManifestV1 segmentManifestV1 = mapper.readValue(manifest, SegmentManifestV1.class);
+            final FetchChunkEnumeration fetchChunkEnumeration = new FetchChunkEnumeration(
+                chunkManager,
+                remoteLogSegmentMetadata,
+                segmentManifestV1,
+                startPosition,
+                endPosition);
+            return new SequenceInputStream(fetchChunkEnumeration);
+        } catch (final IOException e) {
+            throw new RemoteStorageException(e);
+        }
     }
 
     @Override
     public InputStream fetchIndex(final RemoteLogSegmentMetadata remoteLogSegmentMetadata,
                                   final IndexType indexType) throws RemoteStorageException {
-        throw new UnsupportedOperationException();
+        try {
+            return objectStorageFactory.fileFetcher()
+                .fetch(objectKey.key(remoteLogSegmentMetadata, ObjectKey.Suffix.fromIndexType(indexType)));
+        } catch (final IOException e) {
+            // TODO: should be aligned with upstream implementation
+            if (indexType == TRANSACTION) {
+                return null;
+            } else {
+                throw new RemoteStorageException(e);
+            }
+        }
+
     }
 
     @Override
     public void deleteLogSegmentData(final RemoteLogSegmentMetadata remoteLogSegmentMetadata)
         throws RemoteStorageException {
-        throw new UnsupportedOperationException();
+        try {
+            for (final ObjectKey.Suffix suffix : ObjectKey.Suffix.values()) {
+                objectStorageFactory.fileDeleter()
+                    .delete(objectKey.key(remoteLogSegmentMetadata, suffix));
+            }
+        } catch (final IOException e) {
+            throw new RemoteStorageException(e);
+        }
     }
 
     @Override
-    public void close() throws IOException {
-        throw new UnsupportedOperationException();
+    public void close() {
     }
 }
