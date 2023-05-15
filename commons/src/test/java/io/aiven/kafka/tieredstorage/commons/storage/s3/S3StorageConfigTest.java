f763cbb5008b23a4d07e195fa2a22f7b835c482b
Jorge Esteban Quilcate Otoya <jorge.quilcate@aiven.io>
2023-05-15 12:42:28 +0300

    fix: use string literals on tests

--- commons/src/test/java/io/aiven/kafka/tieredstorage/commons/storage/s3/S3StorageConfigTest.java
+++ commons/src/test/java/io/aiven/kafka/tieredstorage/commons/storage/s3/S3StorageConfigTest.java
@@ -37,7 +37,7 @@ class S3StorageConfigTest {
     @Test
     void minimalConfig() {
         final String bucketName = "b1";
-        final Map<String, Object> configs = Map.of(S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName);
+        final Map<String, Object> configs = Map.of("s3.bucket.name", bucketName);
         final S3StorageConfig config = new S3StorageConfig(configs);
         assertThat(config.bucketName()).isEqualTo(bucketName);
     }
@@ -50,14 +50,14 @@ class S3StorageConfigTest {
         final String region = Regions.US_EAST_2.getName();
         final String minioUrl = "http://minio";
         final Map<String, Object> configs = Map.of(
-            S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName,
-            S3StorageConfig.S3_REGION_CONFIG, region,
-            S3StorageConfig.S3_ENDPOINT_URL_CONFIG, minioUrl);
+            "s3.bucket.name", bucketName,
+            "s3.region", region,
+            "s3.endpoint.url", minioUrl);
         final S3StorageConfig config = new S3StorageConfig(configs);
         assertThat(config.bucketName()).isEqualTo(bucketName);
-        assertThat(config.getString(S3StorageConfig.S3_REGION_CONFIG)).isEqualTo(region);
-        assertThat(config.getString(S3StorageConfig.S3_ENDPOINT_URL_CONFIG)).isEqualTo(minioUrl);
-        assertThat(config.getClass(S3StorageConfig.AWS_CREDENTIALS_PROVIDER_CLASS_CONFIG)).isNull();
+        assertThat(config.getString("s3.region")).isEqualTo(region);
+        assertThat(config.getString("s3.endpoint.url")).isEqualTo(minioUrl);
+        assertThat(config.getClass("aws.credentials.provider.class")).isNull();
 
         final AWSCredentialsProvider credentialsProvider = config.credentialsProvider();
         assertThat(credentialsProvider).isNull();
@@ -71,15 +71,15 @@ class S3StorageConfigTest {
         final String minioUrl = "http://minio";
         final String customConfigProvider = EnvironmentVariableCredentialsProvider.class.getName();
         final Map<String, Object> configs = Map.of(
-            S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName,
-            S3StorageConfig.S3_REGION_CONFIG, region,
-            S3StorageConfig.S3_ENDPOINT_URL_CONFIG, minioUrl,
-            S3StorageConfig.AWS_CREDENTIALS_PROVIDER_CLASS_CONFIG, customConfigProvider);
+            "s3.bucket.name", bucketName,
+            "s3.region", region,
+            "s3.endpoint.url", minioUrl,
+            "aws.credentials.provider.class", customConfigProvider);
         final S3StorageConfig config = new S3StorageConfig(configs);
         assertThat(config.bucketName()).isEqualTo(bucketName);
-        assertThat(config.getString(S3StorageConfig.S3_REGION_CONFIG)).isEqualTo(region);
-        assertThat(config.getString(S3StorageConfig.S3_ENDPOINT_URL_CONFIG)).isEqualTo(minioUrl);
-        assertThat(config.getClass(S3StorageConfig.AWS_CREDENTIALS_PROVIDER_CLASS_CONFIG).getName())
+        assertThat(config.getString("s3.region")).isEqualTo(region);
+        assertThat(config.getString("s3.endpoint.url")).isEqualTo(minioUrl);
+        assertThat(config.getClass("aws.credentials.provider.class").getName())
             .isEqualTo(customConfigProvider);
 
         final AWSCredentialsProvider credentialsProvider = config.credentialsProvider();
@@ -95,17 +95,17 @@ class S3StorageConfigTest {
         final String username = "username";
         final String password = "password";
         final Map<String, Object> configs = Map.of(
-            S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName,
-            S3StorageConfig.S3_REGION_CONFIG, region,
-            S3StorageConfig.S3_ENDPOINT_URL_CONFIG, minioUrl,
-            S3StorageConfig.AWS_ACCESS_KEY_ID_CONFIG, username,
-            S3StorageConfig.AWS_SECRET_ACCESS_KEY_CONFIG, password);
+            "s3.bucket.name", bucketName,
+            "s3.region", region,
+            "s3.endpoint.url", minioUrl,
+            "aws.access.key.id", username,
+            "aws.secret.access.key", password);
         final S3StorageConfig config = new S3StorageConfig(configs);
         assertThat(config.bucketName()).isEqualTo(bucketName);
-        assertThat(config.getString(S3StorageConfig.S3_REGION_CONFIG)).isEqualTo(region);
-        assertThat(config.getString(S3StorageConfig.S3_ENDPOINT_URL_CONFIG)).isEqualTo(minioUrl);
-        assertThat(config.getPassword(S3StorageConfig.AWS_ACCESS_KEY_ID_CONFIG).value()).isEqualTo(username);
-        assertThat(config.getPassword(S3StorageConfig.AWS_SECRET_ACCESS_KEY_CONFIG).value()).isEqualTo(password);
+        assertThat(config.getString("s3.region")).isEqualTo(region);
+        assertThat(config.getString("s3.endpoint.url")).isEqualTo(minioUrl);
+        assertThat(config.getPassword("aws.access.key.id").value()).isEqualTo(username);
+        assertThat(config.getPassword("aws.secret.access.key").value()).isEqualTo(password);
 
         final AWSCredentialsProvider credentialsProvider = config.credentialsProvider();
         assertThat(credentialsProvider).isInstanceOf(AWSStaticCredentialsProvider.class);
@@ -120,17 +120,17 @@ class S3StorageConfigTest {
         final String username = "username";
         final String password = "password";
         assertThatThrownBy(() -> new S3StorageConfig(Map.of(
-            S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName,
-            S3StorageConfig.S3_REGION_CONFIG, region,
-            S3StorageConfig.S3_ENDPOINT_URL_CONFIG, minioUrl,
-            S3StorageConfig.AWS_ACCESS_KEY_ID_CONFIG, username)))
+            "s3.bucket.name", bucketName,
+            "s3.region", region,
+            "s3.endpoint.url", minioUrl,
+            "aws.access.key.id", username)))
             .isInstanceOf(ConfigException.class)
             .hasMessage("aws.access.key.id and aws.secret.access.key must be defined together");
         assertThatThrownBy(() -> new S3StorageConfig(Map.of(
-            S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName,
-            S3StorageConfig.S3_REGION_CONFIG, region,
-            S3StorageConfig.S3_ENDPOINT_URL_CONFIG, minioUrl,
-            S3StorageConfig.AWS_SECRET_ACCESS_KEY_CONFIG, password)))
+            "s3.bucket.name", bucketName,
+            "s3.region", region,
+            "s3.endpoint.url", minioUrl,
+            "aws.secret.access.key", password)))
             .isInstanceOf(ConfigException.class)
             .hasMessage("aws.access.key.id and aws.secret.access.key must be defined together");
     }
@@ -145,12 +145,12 @@ class S3StorageConfigTest {
         final String username = "username";
         final String password = "password";
         final Map<String, Object> configs = Map.of(
-            S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName,
-            S3StorageConfig.S3_REGION_CONFIG, region,
-            S3StorageConfig.S3_ENDPOINT_URL_CONFIG, minioUrl,
-            S3StorageConfig.AWS_CREDENTIALS_PROVIDER_CLASS_CONFIG, customConfigProvider,
-            S3StorageConfig.AWS_ACCESS_KEY_ID_CONFIG, username,
-            S3StorageConfig.AWS_SECRET_ACCESS_KEY_CONFIG, password);
+            "s3.bucket.name", bucketName,
+            "s3.region", region,
+            "s3.endpoint.url", minioUrl,
+            "aws.credentials.provider.class", customConfigProvider,
+            "aws.access.key.id", username,
+            "aws.secret.access.key", password);
         assertThatThrownBy(() -> new S3StorageConfig(configs))
             .isInstanceOf(ConfigException.class)
             .hasMessage("Either  static credential pair aws.access.key.id and aws.secret.access.key "
@@ -171,7 +171,7 @@ class S3StorageConfigTest {
     @Test
     void shouldCreateClientWithMinimalConfig() {
         final String bucketName = "b1";
-        final Map<String, Object> configs = Map.of(S3StorageConfig.S3_BUCKET_NAME_CONFIG, bucketName);
+        final Map<String, Object> configs = Map.of("s3.bucket.name", bucketName);
         final S3StorageConfig config = new S3StorageConfig(configs);
         assertThat(config.bucketName()).isEqualTo(bucketName);
 
