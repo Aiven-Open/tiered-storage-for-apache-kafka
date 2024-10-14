daec916e9577a5d527272cfff3090f3bb5006525
Jorge Esteban Quilcate Otoya <jorge.quilcate@aiven.io>
2024-10-14 18:40:44 +0300

    refactor: add metrics registry for all components
    Most components had metric names hardcoded. Adding a metrics registry for each component to contain all metric name templates, and export them for document generation.

---
+++ core/src/main/java/io/aiven/kafka/tieredstorage/metrics/ThreadPoolMonitorMetricsRegistry.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2024 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.tieredstorage.metrics;
+
+import java.util.List;
+
+import org.apache.kafka.common.MetricNameTemplate;
+
+public class ThreadPoolMonitorMetricsRegistry {
+
+    static final String ACTIVE_THREADS = "active-thread-count";
+    private static final String ACTIVE_THREADS_TOTAL = ACTIVE_THREADS + "-total";
+    static final String RUNNING_THREADS = "running-thread-count";
+    private static final String RUNNING_THREADS_TOTAL = RUNNING_THREADS + "-total";
+    static final String POOL_SIZE = "pool-size";
+    private static final String POOL_SIZE_TOTAL = POOL_SIZE + "-total";
+    static final String PARALLELISM = "parallelism";
+    private static final String PARALLELISM_TOTAL = PARALLELISM + "-total";
+    static final String QUEUED_TASK_COUNT = "queued-task-count";
+    private static final String QUEUED_TASK_COUNT_TOTAL = QUEUED_TASK_COUNT + "-total";
+    static final String STEAL_TASK_COUNT = "steal-task-count";
+    private static final String STEAL_TASK_COUNT_TOTAL = STEAL_TASK_COUNT + "-total";
+
+    final String groupName;
+    final MetricNameTemplate activeThreadsTotalMetricName;
+    final MetricNameTemplate runningThreadsTotalMetricName;
+    final MetricNameTemplate poolSizeTotalMetricName;
+    final MetricNameTemplate parallelismTotalMetricName;
+    final MetricNameTemplate queuedTaskCountTotalMetricName;
+    final MetricNameTemplate stealTaskCountTotalMetricName;
+
+    public ThreadPoolMonitorMetricsRegistry(final String groupName) {
+        this.groupName = groupName;
+        activeThreadsTotalMetricName = new MetricNameTemplate(ACTIVE_THREADS_TOTAL, groupName, "");
+        runningThreadsTotalMetricName = new MetricNameTemplate(RUNNING_THREADS_TOTAL, groupName, "");
+        poolSizeTotalMetricName = new MetricNameTemplate(POOL_SIZE_TOTAL, groupName, "");
+        parallelismTotalMetricName = new MetricNameTemplate(PARALLELISM_TOTAL, groupName, "");
+        queuedTaskCountTotalMetricName = new MetricNameTemplate(QUEUED_TASK_COUNT_TOTAL, groupName, "");
+        stealTaskCountTotalMetricName = new MetricNameTemplate(STEAL_TASK_COUNT_TOTAL, groupName, "");
+    }
+
+    public List<MetricNameTemplate> all() {
+        return List.of(
+            activeThreadsTotalMetricName,
+            runningThreadsTotalMetricName,
+            poolSizeTotalMetricName,
+            parallelismTotalMetricName,
+            queuedTaskCountTotalMetricName,
+            stealTaskCountTotalMetricName
+        );
+    }
+}
---
+++ storage/gcs/src/main/java/io/aiven/kafka/tieredstorage/storage/gcs/MetricRegistry.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2024 Aiven Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.aiven.kafka.tieredstorage.storage.gcs;
+
+import java.util.List;
+
+import org.apache.kafka.common.MetricNameTemplate;
+
+public class MetricRegistry {
+    static final String METRIC_GROUP = "gcs-client-metrics";
+    static final String OBJECT_METADATA_GET = "object-metadata-get";
+    static final String OBJECT_METADATA_GET_RATE = OBJECT_METADATA_GET + "-rate";
+    static final String OBJECT_METADATA_GET_TOTAL = OBJECT_METADATA_GET + "-total";
+    static final String OBJECT_GET = "object-get";
+    static final String OBJECT_GET_RATE = OBJECT_GET + "-rate";
+    static final String OBJECT_GET_TOTAL = OBJECT_GET + "-total";
+    static final String OBJECT_DELETE = "object-delete";
+    static final String OBJECT_DELETE_RATE = OBJECT_DELETE + "-rate";
+    static final String OBJECT_DELETE_TOTAL = OBJECT_DELETE + "-total";
+    static final String RESUMABLE_UPLOAD_INITIATE = "resumable-upload-initiate";
+    static final String RESUMABLE_UPLOAD_INITIATE_RATE = RESUMABLE_UPLOAD_INITIATE + "-rate";
+    static final String RESUMABLE_UPLOAD_INITIATE_TOTAL = RESUMABLE_UPLOAD_INITIATE + "-total";
+    static final String RESUMABLE_CHUNK_UPLOAD = "resumable-chunk-upload";
+    static final String RESUMABLE_CHUNK_UPLOAD_RATE = RESUMABLE_CHUNK_UPLOAD + "-rate";
+    static final String RESUMABLE_CHUNK_UPLOAD_TOTAL = RESUMABLE_CHUNK_UPLOAD + "-total";
+
+    static final MetricNameTemplate OBJECT_METADATA_GET_RATE_METRIC_NAME =
+        new MetricNameTemplate(OBJECT_METADATA_GET_RATE, METRIC_GROUP, "");
+    static final MetricNameTemplate OBJECT_METADATA_GET_TOTAL_METRIC_NAME =
+        new MetricNameTemplate(OBJECT_METADATA_GET_TOTAL, METRIC_GROUP, "");
+    static final MetricNameTemplate OBJECT_GET_RATE_METRIC_NAME =
+        new MetricNameTemplate(OBJECT_GET_RATE, METRIC_GROUP, "");
+    static final MetricNameTemplate OBJECT_GET_TOTAL_METRIC_NAME =
+        new MetricNameTemplate(OBJECT_GET_TOTAL, METRIC_GROUP, "");
+    static final MetricNameTemplate OBJECT_DELETE_RATE_METRIC_NAME =
+        new MetricNameTemplate(OBJECT_DELETE_RATE, METRIC_GROUP, "");
+    static final MetricNameTemplate OBJECT_DELETE_TOTAL_METRIC_NAME =
+        new MetricNameTemplate(OBJECT_DELETE_TOTAL, METRIC_GROUP, "");
+    static final MetricNameTemplate RESUMABLE_UPLOAD_INITIATE_RATE_METRIC_NAME =
+        new MetricNameTemplate(RESUMABLE_UPLOAD_INITIATE_RATE, METRIC_GROUP, "");
+    static final MetricNameTemplate RESUMABLE_UPLOAD_INITIATE_TOTAL_METRIC_NAME =
+        new MetricNameTemplate(RESUMABLE_UPLOAD_INITIATE_TOTAL, METRIC_GROUP, "");
+    static final MetricNameTemplate RESUMABLE_CHUNK_UPLOAD_RATE_METRIC_NAME =
+        new MetricNameTemplate(RESUMABLE_CHUNK_UPLOAD_RATE, METRIC_GROUP, "");
+    static final MetricNameTemplate RESUMABLE_CHUNK_UPLOAD_TOTAL_METRIC_NAME =
+        new MetricNameTemplate(RESUMABLE_CHUNK_UPLOAD_TOTAL, METRIC_GROUP, "");
+
+    public List<MetricNameTemplate> all() {
+        return List.of(
+            OBJECT_METADATA_GET_RATE_METRIC_NAME,
+            OBJECT_METADATA_GET_TOTAL_METRIC_NAME,
+            OBJECT_GET_RATE_METRIC_NAME,
+            OBJECT_GET_TOTAL_METRIC_NAME,
+            OBJECT_DELETE_RATE_METRIC_NAME,
+            OBJECT_DELETE_TOTAL_METRIC_NAME,
+            RESUMABLE_UPLOAD_INITIATE_RATE_METRIC_NAME,
+            RESUMABLE_UPLOAD_INITIATE_TOTAL_METRIC_NAME,
+            RESUMABLE_CHUNK_UPLOAD_RATE_METRIC_NAME,
+            RESUMABLE_CHUNK_UPLOAD_TOTAL_METRIC_NAME
+        );
+    }
+}
