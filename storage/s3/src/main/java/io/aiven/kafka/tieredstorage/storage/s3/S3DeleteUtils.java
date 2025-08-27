710357c01ea67427c3195abf088b2a4fe98784ac
jianbin <jianbin@apache.org>
2025-08-27 17:39:16 +0800

    feat: add S3 delete utilities and support for Content-MD5 header in delete requests

--- storage/s3/src/main/java/io/aiven/kafka/tieredstorage/storage/s3/S3DeleteUtils.java
+++ storage/s3/src/main/java/io/aiven/kafka/tieredstorage/storage/s3/S3DeleteUtils.java
@@ -16,14 +16,13 @@
 
 package io.aiven.kafka.tieredstorage.storage.s3;
 
-import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
-
 import java.security.MessageDigest;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.List;
 import java.util.Objects;
 
+import software.amazon.awssdk.services.s3.model.ObjectIdentifier;
 
 /**
  * Helper utilities for S3 delete operations.
